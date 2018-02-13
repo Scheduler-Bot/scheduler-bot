8629560eb1bc4c96c6e03fad54f3f35dc7644f93
Alexander Kravchuk <akravch92@gmail.com>
2018-02-13 13:38:12 +0300

    Add ScheduledMessageConfiguration.

---
+++ SchedulerBot/SchedulerBot.Database.Core/Configurations/ScheduledMessageConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchedulerBot.Database.Entities;
+
+namespace SchedulerBot.Database.Core.Configurations
+{
+	public class ScheduledMessageConfiguration : IEntityTypeConfiguration<ScheduledMessage>
+	{
+		public void Configure(EntityTypeBuilder<ScheduledMessage> builder)
+		{
+			builder.HasKey(message => message.Id);
+			builder.Property(message => message.Id).ValueGeneratedOnAdd();
+			builder.Property(message => message.ConversationId).IsRequired();
+			builder.Property(message => message.Message).IsRequired();
+		}
+	}
+}
