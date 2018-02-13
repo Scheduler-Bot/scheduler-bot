d7212dbdd43340f9b713a2aaf032aec52a65346e
Alexander Kravchuk <akravch92@gmail.com>
2018-02-13 11:37:00 +0300

    Create MessagesController with the body from the quick start.

---
+++ SchedulerBot/SchedulerBot/Controllers/MessagesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+
+namespace SchedulerBot.Controllers
+{
+	[Route("api/[controller]")]
+	public class MessagesController : Controller
+	{
+		private readonly IConfiguration configuration;
+
+		public MessagesController(IConfiguration configuration)
+		{
+			this.configuration = configuration;
+		}
+
+		[Authorize(Roles = "Bot")]
+		[HttpPost]
+		public async Task<OkResult> Post([FromBody] Activity activity)
+		{
+			if (activity.Type == ActivityTypes.Message)
+			{
+				//MicrosoftAppCredentials.TrustServiceUrl(activity.ServiceUrl);
+				var appCredentials = new MicrosoftAppCredentials(configuration["MicrosoftAppId"], configuration["MicrosoftAppPassword"]);
+				var connector = new ConnectorClient(new Uri(activity.ServiceUrl), appCredentials);
+
+				// return our reply to the user
+				var reply = activity.CreateReply("HelloWorld");
+				await connector.Conversations.ReplyToActivityAsync(reply);
+			}
+			else
+			{
+				//HandleSystemMessage(activity);
+			}
+
+			return Ok();
+		}
+	}
+}
