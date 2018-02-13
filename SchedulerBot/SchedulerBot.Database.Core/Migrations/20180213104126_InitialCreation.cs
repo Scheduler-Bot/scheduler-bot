6432b093ffc5afc322ad9a4dfb22170b9e3ddd7f
Alexander Kravchuk <akravch92@gmail.com>
2018-02-13 13:42:56 +0300

    Add initial migration.

---
+++ SchedulerBot/SchedulerBot.Database.Core/Migrations/20180213104126_InitialCreation.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace SchedulerBot.Database.Core.Migrations
+{
+    public partial class InitialCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ScheduledMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ConversationId = table.Column<string>(nullable: true),
+                    Message = table.Column<string>(nullable: true),
+                    PerformAt = table.Column<DateTime>(nullable: false),
+                    State = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduledMessages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScheduledMessages");
+        }
+    }
+}
