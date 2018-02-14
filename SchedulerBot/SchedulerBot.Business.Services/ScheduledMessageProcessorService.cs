b1901a638a58f03c016ee10f11a95f44c1cd59d4
Alexander Kravchuk <akravch92@gmail.com>
2018-02-14 12:09:41 +0300

    Go throught the scheduled messages in ScheduledMessageProcessorService.

--- SchedulerBot/SchedulerBot.Business.Services/ScheduledMessageProcessorService.cs
+++ SchedulerBot/SchedulerBot.Business.Services/ScheduledMessageProcessorService.cs
@@ -1,24 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SchedulerBot.Database.Core;
+using SchedulerBot.Database.Entities;
+using SchedulerBot.Infrastructure.Interfaces;
 
 namespace SchedulerBot.Business.Services
 {
 	public sealed class ScheduledMessageProcessorService : IHostedService, IDisposable
 	{
 		private readonly IServiceScopeFactory scopeFactory;
+		private readonly IScheduleParser scheduleParser;
 		private readonly TimeSpan pollingInterval;
 		private readonly CancellationTokenSource serviceCancellationTokenSource;
 		private readonly CancellationToken serviceCancellationToken;
 
-		public ScheduledMessageProcessorService(IServiceScopeFactory scopeFactory, IConfiguration configuration)
+		public ScheduledMessageProcessorService(IServiceScopeFactory scopeFactory, IConfiguration configuration, IScheduleParser scheduleParser)
 		{
 			this.scopeFactory = scopeFactory;
+			this.scheduleParser = scheduleParser;
 			pollingInterval = TimeSpan.Parse(configuration["MessageProcessingInterval"], CultureInfo.InvariantCulture);
 			serviceCancellationTokenSource = new CancellationTokenSource();
 			serviceCancellationToken = serviceCancellationTokenSource.Token;
@@ -50,9 +57,17 @@ namespace SchedulerBot.Business.Services
 		{
 			using (IServiceScope scope = scopeFactory.CreateScope())
 			{
+				DateTime currentTime = DateTime.UtcNow;
 				SchedulerBotContext context = scope.ServiceProvider.GetRequiredService<SchedulerBotContext>();
+				IQueryable<ScheduledMessage> messagesToProcess = context
+					.ScheduledMessages
+					.Include(message => message.Logs)
+					.Where(message => ShouldSendMessage(message, currentTime));
 
-				// TODO: The logic.
+				foreach (ScheduledMessage scheduledMessage in messagesToProcess)
+				{
+					// TODO: send message
+				}
 
 				await context.SaveChangesAsync(serviceCancellationToken);
 			}
@@ -68,6 +83,16 @@ namespace SchedulerBot.Business.Services
 			{
 				// Just swallow it.
 			}
+		}
+
+		private bool ShouldSendMessage(ScheduledMessage scheduledMessage, DateTime currentTime)
+		{
+			ICollection<ScheduledMessageLog> logs = scheduledMessage.Logs;
+			DateTime lastOccurence = logs?.Max(log => log.CreatedOn) ?? DateTime.MinValue;
+			ISchedule messageSchedule = scheduleParser.Parse(scheduledMessage.Schedule, lastOccurence);
+			DateTime nextOccurence = messageSchedule.NextOccurence;
+
+			return nextOccurence >= lastOccurence && nextOccurence <= currentTime;
 		}
 	}
 }
