3bc399522614f4cd40c3bccc32765d453d2881ba
Alexander Kravchuk <akravch92@gmail.com>
2018-02-14 11:26:14 +0300

    Make MessagesController schedule messages if they appear in correct format.

--- SchedulerBot/SchedulerBot/Controllers/MessagesController.cs
+++ SchedulerBot/SchedulerBot/Controllers/MessagesController.cs
@@ -6,17 +6,24 @@ using Microsoft.Bot.Connector;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Configuration;
+using SchedulerBot.Database.Core;
+using SchedulerBot.Database.Entities;
+using SchedulerBot.Infrastructure.Interfaces;
 
 namespace SchedulerBot.Controllers
 {
 	[Route("api/[controller]")]
 	public class MessagesController : Controller
 	{
+		private readonly SchedulerBotContext context;
 		private readonly IConfiguration configuration;
+		private readonly IScheduleParser scheduleParser;
 
-		public MessagesController(IConfiguration configuration)
+		public MessagesController(SchedulerBotContext context, IConfiguration configuration, IScheduleParser scheduleParser)
 		{
+			this.context = context;
 			this.configuration = configuration;
+			this.scheduleParser = scheduleParser;
 		}
 
 		[Authorize(Roles = "Bot")]
@@ -25,23 +32,48 @@ namespace SchedulerBot.Controllers
 		{
 			if (activity.Type == ActivityTypes.Message)
 			{
-				//MicrosoftAppCredentials.TrustServiceUrl(activity.ServiceUrl);
-				string appId = configuration[MicrosoftAppCredentials.MicrosoftAppIdKey];
-				string appPassword = configuration[MicrosoftAppCredentials.MicrosoftAppPasswordKey];
-				MicrosoftAppCredentials appCredentials = new MicrosoftAppCredentials(appId, appPassword);
-				ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl), appCredentials);
+				string replyText;
+				string textSchedule = activity.Text;
 
-				// return our reply to the user
-				Activity reply = activity.CreateReply("HelloWorld");
+				if (scheduleParser.TryParse(textSchedule, out ISchedule schedule))
+				{
+					await AddScheduledMessageAsync(activity, schedule);
 
-				await connector.Conversations.ReplyToActivityAsync(reply);
-			}
-			else
-			{
-				//HandleSystemMessage(activity);
+					replyText = $"Created an event with the following schedule: \"{textSchedule}\"";
+				}
+				else
+				{
+					replyText = $"Cannot recognize schedule \"{textSchedule}\"";
+				}
+
+				await ReplyAsync(activity, replyText);
 			}
 
 			return Ok();
+		}
+
+		private Task<ResourceResponse> ReplyAsync(Activity activity, string replyText)
+		{
+			string appId = configuration[MicrosoftAppCredentials.MicrosoftAppIdKey];
+			string appPassword = configuration[MicrosoftAppCredentials.MicrosoftAppPasswordKey];
+			MicrosoftAppCredentials appCredentials = new MicrosoftAppCredentials(appId, appPassword);
+			ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl), appCredentials);
+			Activity reply = activity.CreateReply(replyText);
+
+			return connector.Conversations.ReplyToActivityAsync(reply);
+		}
+
+		private async Task AddScheduledMessageAsync(Activity activity, ISchedule schedule)
+		{
+			ScheduledMessage scheduledMessage = new ScheduledMessage
+			{
+				ConversationId = activity.Conversation.Id,
+				Text = "Hello!",
+				Schedule = schedule.Text
+			};
+
+			await context.ScheduledMessages.AddAsync(scheduledMessage);
+			await context.SaveChangesAsync();
 		}
 	}
 }
