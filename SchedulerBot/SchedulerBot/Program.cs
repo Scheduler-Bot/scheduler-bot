cfb7560066336e7525a0783a90fef1929e1716e2
Pavel Ryabov <prpbox@gmail.com>
2018-02-15 09:17:28 +0300

    Work with Key Vault secrets

--- SchedulerBot/SchedulerBot/Program.cs
+++ SchedulerBot/SchedulerBot/Program.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace SchedulerBot
 {
@@ -12,7 +17,22 @@ namespace SchedulerBot
 
 		public static IWebHost BuildWebHost(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
+				.ConfigureAppConfiguration((ctx, builder) =>
+				{
+					string keyVaultEndpoint = GetKeyVaultEndpoint();
+					if (!string.IsNullOrEmpty(keyVaultEndpoint))
+					{
+						AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+						KeyVaultClient keyVaultClient = new KeyVaultClient(
+							new KeyVaultClient.AuthenticationCallback(
+								azureServiceTokenProvider.KeyVaultTokenCallback));
+						builder.AddAzureKeyVault(
+							keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+					}
+				})
 				.UseStartup<Startup>()
 				.Build();
+
+		private static string GetKeyVaultEndpoint() => Environment.GetEnvironmentVariable("KEYVAULT_ENDPOINT");
 	}
 }
