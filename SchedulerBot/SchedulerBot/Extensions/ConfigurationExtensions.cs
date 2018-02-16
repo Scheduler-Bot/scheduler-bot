290eb1bb68c787940726610a5fe4e0d531449ce4
Alexander Kravchuk <akravch92@gmail.com>
2018-02-16 14:58:16 +0300

    Create ConfigurationExtensions and use from Program.

---
+++ SchedulerBot/SchedulerBot/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.DependencyInjection;
+using SchedulerBot.Database.Core;
+
+namespace SchedulerBot.Extensions
+{
+	public static class ConfigurationExtensions
+	{
+		public static IConfigurationBuilder AddSecrets(this IConfigurationBuilder builder)
+		{
+			string keyVaultEndpoint = GetKeyVaultEndpoint();
+
+			if (!string.IsNullOrEmpty(keyVaultEndpoint))
+			{
+				AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+				KeyVaultClient keyVaultClient = new KeyVaultClient(
+					new KeyVaultClient.AuthenticationCallback(
+						azureServiceTokenProvider.KeyVaultTokenCallback));
+				builder.AddAzureKeyVault(
+					keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+			}
+			else
+			{
+				builder.AddUserSecrets<Startup>();
+			}
+
+			return builder;
+		}
+
+		public static IWebHost EnsureDatabaseMigrated(this IWebHost host)
+		{
+			using (IServiceScope scope = host.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+			{
+				IServiceProvider scopeServiceProvider = scope.ServiceProvider;
+				SchedulerBotContext context = scopeServiceProvider.GetRequiredService<SchedulerBotContext>();
+
+				context.Database.Migrate();
+			}
+
+			return host;
+		}
+
+		private static string GetKeyVaultEndpoint() => Environment.GetEnvironmentVariable("KEYVAULT_ENDPOINT");
+	}
+}
--- SchedulerBot/SchedulerBot/Program.cs
+++ SchedulerBot/SchedulerBot/Program.cs
@@ -1,55 +1,20 @@
-﻿using System;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.AzureKeyVault;
-using Microsoft.Extensions.DependencyInjection;
-using SchedulerBot.Database.Core;
+using SchedulerBot.Extensions;
 
 namespace SchedulerBot
 {
 	public class Program
 	{
-		public static void Main(string[] args)
-		{
-			IWebHost host = BuildWebHost(args);
-
-			using (IServiceScope scope = host.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
-			{
-				IServiceProvider scopeServiceProvider = scope.ServiceProvider;
-				SchedulerBotContext context = scopeServiceProvider.GetRequiredService<SchedulerBotContext>();
-
-				context.Database.Migrate();
-			}
-
-			host.Run();
-		}
+		public static void Main(string[] args) =>
+			BuildWebHost(args)
+				.EnsureDatabaseMigrated()
+				.Run();
 
 		public static IWebHost BuildWebHost(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
-				.ConfigureAppConfiguration((ctx, builder) =>
-				{
-					string keyVaultEndpoint = GetKeyVaultEndpoint();
-					if (!string.IsNullOrEmpty(keyVaultEndpoint))
-					{
-						AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
-						KeyVaultClient keyVaultClient = new KeyVaultClient(
-							new KeyVaultClient.AuthenticationCallback(
-								azureServiceTokenProvider.KeyVaultTokenCallback));
-						builder.AddAzureKeyVault(
-							keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
-					}
-					else
-					{
-						builder.AddUserSecrets<Startup>();
-					}
-				})
+				.ConfigureAppConfiguration((ctx, builder) => builder.AddSecrets())
 				.UseStartup<Startup>()
 				.Build();
-
-		private static string GetKeyVaultEndpoint() => Environment.GetEnvironmentVariable("KEYVAULT_ENDPOINT");
 	}
 }
