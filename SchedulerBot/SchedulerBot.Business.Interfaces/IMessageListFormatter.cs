227cc80cf4f5090549ab56074d1e629dd28f6bf0
Alexander Kravchuk <akravch92@gmail.com>
2018-02-23 13:21:44 +0300

    Add MessageListFormatter.

---
+++ SchedulerBot/SchedulerBot.Business.Interfaces/IMessageListFormatter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Microsoft.Bot.Schema;
+using SchedulerBot.Database.Entities;
+
+namespace SchedulerBot.Business.Interfaces
+{
+	public interface IMessageListFormatter
+	{
+		string Format(IEnumerable<ScheduledMessage> scheduledMessages, Activity activity);
+	}
+}
