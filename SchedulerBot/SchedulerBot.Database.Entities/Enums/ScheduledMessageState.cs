1aec956fb15047a71e06d6d06eeb5e7c0b282aa7
Alexander Kravchuk <akravch92@gmail.com>
2018-03-13 15:40:46 +0300

    Add comments in the SchedulerBot.Database.Entities project.

--- SchedulerBot/SchedulerBot.Database.Entities/Enums/ScheduledMessageState.cs
+++ SchedulerBot/SchedulerBot.Database.Entities/Enums/ScheduledMessageState.cs
@@ -1,8 +1,18 @@
 ﻿namespace SchedulerBot.Database.Entities.Enums
 {
+	/// <summary>
+	/// Describes the scheduled message state.
+	/// </summary>
 	public enum ScheduledMessageState
 	{
+		/// <summary>
+		/// The message is active and is being sent accordingly to its schedule.
+		/// </summary>
 		Active = 1,
+
+		/// <summary>
+		/// The message has been deleted and is not being sent.
+		/// </summary>
 		Deleted = 2
 	}
 }
--- SchedulerBot/SchedulerBot.Database.Entities/ScheduledMessageDetails.cs
+++ SchedulerBot/SchedulerBot.Database.Entities/ScheduledMessageDetails.cs
@@ -2,28 +2,64 @@
 
 namespace SchedulerBot.Database.Entities
 {
+	/// <summary>
+	/// Represents scheduled message details.
+	/// </summary>
 	public class ScheduledMessageDetails
 	{
+		/// <summary>
+		/// Gets or sets the associated scheduled message identifier.
+		/// </summary>
 		public Guid ScheduledMessageId { get; set; }
 
+		/// <summary>
+		/// Gets or sets the associated scheduled message.
+		/// </summary>
 		public ScheduledMessage ScheduledMessage { get; set; }
 
+		/// <summary>
+		/// Gets or sets the service URL.
+		/// </summary>
 		public string ServiceUrl { get; set; }
 
+		/// <summary>
+		/// Gets or sets the identifier of the message sender - the bot.
+		/// </summary>
 		public string FromId { get; set; }
 
+		/// <summary>
+		/// Gets or sets the name of the message sender - the bot.
+		/// </summary>
 		public string FromName { get; set; }
 
+		/// <summary>
+		/// Gets or sets the recipient identifier.
+		/// </summary>
 		public string RecipientId { get; set; }
 
+		/// <summary>
+		/// Gets or sets the name of the recipient.
+		/// </summary>
 		public string RecipientName { get; set; }
 
+		/// <summary>
+		/// Gets or sets the channel identifier.
+		/// </summary>
 		public string ChannelId { get; set; }
 
+		/// <summary>
+		/// Gets or sets the conversation identifier.
+		/// </summary>
 		public string ConversationId { get; set; }
 
+		/// <summary>
+		/// Gets or sets the locale of the recipient.
+		/// </summary>
 		public string Locale { get; set; }
 
+		/// <summary>
+		/// Gets or sets the UTC offset of the recipient time zone.
+		/// </summary>
 		public TimeSpan? TimeZoneOffset { get; set; }
 	}
 }
