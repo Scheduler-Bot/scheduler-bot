9c84d4dc8ac6f9930210422d25cc1aaa9c124529
Alexander Kravchuk <akravch92@gmail.com>
2018-03-13 16:04:28 +0300

    Add comments for SchedulerBot.Infrastructure.Interfaces project.

--- SchedulerBot/SchedulerBot.Infrastructure.Interfaces/BotConnector/IMessageProcessor.cs
+++ SchedulerBot/SchedulerBot.Infrastructure.Interfaces/BotConnector/IMessageProcessor.cs
@@ -5,10 +5,26 @@ using SchedulerBot.Database.Entities;
 
 namespace SchedulerBot.Infrastructure.Interfaces.BotConnector
 {
+	/// <summary>
+	/// Defines an interface for processing messages.
+	/// </summary>
 	public interface IMessageProcessor
 	{
+		/// <summary>
+		/// Sends the message asynchronously.
+		/// </summary>
+		/// <param name="scheduledMessage">The scheduled message.</param>
+		/// <param name="cancellationToken">The cancellation token.</param>
+		/// <returns>A task that represents the message sending.</returns>
 		Task SendMessageAsync(ScheduledMessage scheduledMessage, CancellationToken cancellationToken);
 
+		/// <summary>
+		/// Replies to the user message asynchronously.
+		/// </summary>
+		/// <param name="activity">The activity.</param>
+		/// <param name="replyText">The reply text.</param>
+		/// <param name="cancellationToken">The cancellation token.</param>
+		/// <returns>A task that represents the reply.</returns>
 		Task<ResourceResponse> ReplyAsync(Activity activity, string replyText, CancellationToken cancellationToken);
 	}
 }
--- SchedulerBot/SchedulerBot.Infrastructure.Interfaces/Schedule/IScheduleParser.cs
+++ SchedulerBot/SchedulerBot.Infrastructure.Interfaces/Schedule/IScheduleParser.cs
@@ -2,10 +2,27 @@
 
 namespace SchedulerBot.Infrastructure.Interfaces.Schedule
 {
+	/// <summary>
+	/// Describes a mechanism of parsing a schedule in text form.
+	/// </summary>
 	public interface IScheduleParser
 	{
+		/// <summary>
+		/// Converts a string representation of a schedule to an <see cref="ISchedule"/> instance.
+		/// </summary>
+		/// <param name="textSchedule">The text schedule.</param>
+		/// <param name="timeZoneOffset">The time zone offset. If not provided, the schedule is constructed without considering an offset.</param>
+		/// <returns><see cref="ISchedule"/> instance describing the provided schedule.</returns>
 		ISchedule Parse(string textSchedule, TimeSpan? timeZoneOffset);
 
+		/// <summary>
+		/// Converts a string representation of a schedule to an <see cref="ISchedule"/> instance.
+		/// A return value indicates whether the convertion succeeded.
+		/// </summary>
+		/// <param name="textSchedule">The text schedule.</param>
+		/// <param name="timeZoneOffset">The time zone offset. If not provided, the schedule is constructed without considering an offset.</param>
+		/// <param name="schedule">The <see cref="ISchedule"/> instance if the convertion succeeded, otherwise <c>null</c>.</param>
+		/// <returns>A boolean value indicating whether the conversion succeeded.</returns>
 		bool TryParse(string textSchedule, TimeSpan? timeZoneOffset, out ISchedule schedule);
 	}
 }
