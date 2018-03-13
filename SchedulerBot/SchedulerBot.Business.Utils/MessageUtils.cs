ab000527bcad05da47638681539c67202c8c0921
Alexander Kravchuk <akravch92@gmail.com>
2018-03-13 14:49:30 +0300

    Add comments for MessageUtils.

--- SchedulerBot/SchedulerBot.Business.Utils/MessageUtils.cs
+++ SchedulerBot/SchedulerBot.Business.Utils/MessageUtils.cs
@@ -1,7 +1,13 @@
 ﻿namespace SchedulerBot.Business.Utils
 {
+	/// <summary>
+	/// Provides utilities for working with messages.
+	/// </summary>
 	public static class MessageUtils
 	{
+		/// <summary>
+		/// Represents the new line in a message.
+		/// </summary>
 		public static readonly string NewLine = "\n\n";
 	}
 }
