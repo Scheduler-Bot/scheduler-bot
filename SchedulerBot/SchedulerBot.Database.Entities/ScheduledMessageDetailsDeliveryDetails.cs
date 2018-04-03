a273bc5827f6799c3a189e9c37809a3cbb67fe16
Alexander Kravchuk <akravch92@gmail.com>
2018-04-03 13:53:12 +0300

    Add ScheduledMessageDetailsDeliveryDetails.

---
+++ SchedulerBot/SchedulerBot.Database.Entities/ScheduledMessageDetailsDeliveryDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SchedulerBot.Database.Entities
+{
+	/// <summary>
+	/// Enables many-to-many relationship between <see cref="ScheduledMessageDetails"/> and <see cref="ScheduledMessageDeliveryDetails"/>.
+	/// </summary>
+	public class ScheduledMessageDetailsDeliveryDetails
+	{
+		/// <summary>
+		/// Gets or sets the scheduled message details identifier.
+		/// </summary>
+		public Guid DetailsId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the scheduled message details.
+		/// </summary>
+		public ScheduledMessageDetails MessageDetails { get; set; }
+
+		/// <summary>
+		/// Gets or sets the scheduled message delivery details identifier.
+		/// </summary>
+		public Guid DeliveryDetailsId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the scheduled message delivery details.
+		/// </summary>
+		public ScheduledMessageDeliveryDetails DeliveryDetails { get; set; }
+	}
+}
