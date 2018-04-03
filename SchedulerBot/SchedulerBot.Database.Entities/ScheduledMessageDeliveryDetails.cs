a2a8df201e5e1d2a5ce1295d2b3c27986f4bb0b9
Alexander Kravchuk <akravch92@gmail.com>
2018-04-03 13:49:02 +0300

    Add ScheduledMessageDeliveryDetails.

---
+++ SchedulerBot/SchedulerBot.Database.Entities/ScheduledMessageDeliveryDetails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SchedulerBot.Database.Entities
+{
+	/// <summary>
+	/// Holds the information about channel, conversation and service URL.
+	/// </summary>
+	public class ScheduledMessageDeliveryDetails
+	{
+		/// <summary>
+		/// Gets or sets the identifier.
+		/// </summary>
+		public Guid Id { get; set; }
+
+		/// <summary>
+		/// Gets or sets the channel identifier.
+		/// </summary>
+		public string ChannelId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the conversation identifier.
+		/// </summary>
+		public string ConversationId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the service URL address.
+		/// </summary>
+		public string ServiceUrl { get; set; }
+
+		/// <summary>
+		/// Gets or sets the entity creation time.
+		/// </summary>
+		public DateTime CreatedOn { get; set; }
+	}
+}
