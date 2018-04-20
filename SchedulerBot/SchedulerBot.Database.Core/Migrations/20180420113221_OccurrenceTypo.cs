b71d9b033ad8c7f83c353be84abd1598dbe2c0aa
Alexander Kravchuk <akravch92@gmail.com>
2018-04-20 14:34:08 +0300

    Add OccurrenceTypo migration to rename the database column.

---
+++ SchedulerBot/SchedulerBot.Database.Core/Migrations/20180420113221_OccurrenceTypo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchedulerBot.Database.Core.Migrations
+{
+	public partial class OccurrenceTypo : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.RenameColumn(
+				name: "NextOccurence",
+				table: "ScheduledMessageEvents",
+				newName: "NextOccurrence");
+		}
+
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.RenameColumn(
+				name: "NextOccurrence",
+				table: "ScheduledMessageEvents",
+				newName: "NextOccurence");
+		}
+	}
+}
