31a439edbcc9bbe002a5c51e29f009feb487580e
Alexander Kravchuk <akravch92@gmail.com>
2018-04-30 15:51:57 +0300

    Add rough implementation of ManageCommand.

--- SchedulerBot/SchedulerBot.Business.Commands/ManageCommand.cs
+++ SchedulerBot/SchedulerBot.Business.Commands/ManageCommand.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Logging;
 using SchedulerBot.Business.Entities;
+using SchedulerBot.Database.Core;
+using SchedulerBot.Database.Entities;
 
 namespace SchedulerBot.Business.Commands
 {
@@ -12,20 +15,57 @@ namespace SchedulerBot.Business.Commands
 	/// <seealso cref="BotCommand" />
 	public class ManageCommand : BotCommand
 	{
+		private readonly SchedulerBotContext context;
+
 		/// <summary>
 		/// Initializes a new instance of the <see cref="ManageCommand"/> class.
 		/// </summary>
+		/// <param name="context">The context.</param>
 		/// <param name="logger">The logger.</param>
-		public ManageCommand(ILogger<ManageCommand> logger) : base("manage", logger)
+		public ManageCommand(SchedulerBotContext context, ILogger<ManageCommand> logger) : base("manage", logger)
 		{
+			this.context = context;
 		}
 
 		/// <inheritdoc />
-		protected override Task<CommandExecutionResult> ExecuteCoreAsync(Activity activity, string arguments)
+		protected override async Task<CommandExecutionResult> ExecuteCoreAsync(Activity activity, string arguments)
 		{
-			CommandExecutionResult executionResult = CommandExecutionResult.Success("This is supposed to be a link");
+			Logger.LogInformation(
+				"Creating a manage link for channel '{0}' and conversation '{1}'",
+				activity.ChannelId,
+				activity.Conversation.Id);
 
-			return Task.FromResult(executionResult);
+			ManageConversationLink manageConversationLink = CreateManageConversationLink(activity);
+
+			await context.ManageConversationLinks.AddAsync(manageConversationLink);
+			await context.SaveChangesAsync();
+
+			Logger.LogInformation(
+				"Created the manage link '{0}' for channel '{1}' and conversation '{2}'",
+				manageConversationLink.Text,
+				activity.ChannelId,
+				activity.Conversation.Id);
+
+			return CommandExecutionResult.Success(manageConversationLink.Text);
+		}
+
+		private static ManageConversationLink CreateManageConversationLink(Activity activity)
+		{
+			string channelId = activity.ChannelId;
+			string conversationId = activity.Conversation.Id;
+			string linkText = "random-link-text";
+			TimeSpan linkExpirationPeriod = TimeSpan.FromMinutes(20);
+			DateTime linkCreationTime = DateTime.UtcNow;
+			DateTime linkExpirationTime = linkCreationTime + linkExpirationPeriod;
+
+			return new ManageConversationLink
+			{
+				ChannelId = channelId,
+				ConversationId = conversationId,
+				Text = linkText,
+				CreatedOn = linkCreationTime,
+				ExpiresOn = linkExpirationTime
+			};
 		}
 	}
 }
