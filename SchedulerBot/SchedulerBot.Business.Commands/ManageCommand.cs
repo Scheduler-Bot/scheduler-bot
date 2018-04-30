4ef06a9aab4020ed74f19232acdc9efbd4c94614
Alexander Kravchuk <akravch92@gmail.com>
2018-04-30 17:58:14 +0300

    Use RandomByteGenerator for generating links in ManageCommand.

--- SchedulerBot/SchedulerBot.Business.Commands/ManageCommand.cs
+++ SchedulerBot/SchedulerBot.Business.Commands/ManageCommand.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Logging;
 using SchedulerBot.Business.Entities;
 using SchedulerBot.Database.Core;
 using SchedulerBot.Database.Entities;
+using SchedulerBot.Infrastructure.Interfaces.Utils;
 
 namespace SchedulerBot.Business.Commands
 {
@@ -18,20 +19,24 @@ namespace SchedulerBot.Business.Commands
 	public class ManageCommand : BotCommand
 	{
 		private readonly SchedulerBotContext context;
+		private readonly IRandomByteGenerator randomByteGenerator;
 		private readonly TimeSpan linkExpirationPeriod;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="ManageCommand"/> class.
 		/// </summary>
 		/// <param name="context">The context.</param>
+		/// <param name="randomByteGenerator">The random byte generator.</param>
 		/// <param name="configuration">The configuration.</param>
 		/// <param name="logger">The logger.</param>
 		public ManageCommand(
 			SchedulerBotContext context,
+			IRandomByteGenerator randomByteGenerator,
 			IConfiguration configuration,
 			ILogger<ManageCommand> logger) : base("manage", logger)
 		{
 			this.context = context;
+			this.randomByteGenerator = randomByteGenerator;
 			linkExpirationPeriod = TimeSpan.Parse(configuration["Commands:Manage:LinkExpirationPeriod"], CultureInfo.InvariantCulture);
 		}
 
@@ -61,7 +66,8 @@ namespace SchedulerBot.Business.Commands
 		{
 			string channelId = activity.ChannelId;
 			string conversationId = activity.Conversation.Id;
-			string linkText = "random-link-text";
+			byte[] randomBytes = randomByteGenerator.Generate(64);
+			string linkText = Convert.ToBase64String(randomBytes);
 			DateTime linkCreationTime = DateTime.UtcNow;
 			DateTime linkExpirationTime = linkCreationTime + linkExpirationPeriod;
 
