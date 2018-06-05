a294ad45ec7e4a5cdcebb3e102d9bb769411dab9
Pavel Ryabov <prpbox@gmail.com>
2018-06-05 14:25:13 +0300

    Update ListCommand to use unit of work

--- SchedulerBot/SchedulerBot.Database.Interfaces/Repositories/IScheduledMessageRepository.cs
+++ SchedulerBot/SchedulerBot.Database.Interfaces/Repositories/IScheduledMessageRepository.cs
@@ -1,4 +1,7 @@
-﻿using SchedulerBot.Database.Entities;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SchedulerBot.Database.Entities;
+using SchedulerBot.Database.Entities.Enums;
 
 namespace SchedulerBot.Database.Interfaces.Repositories
 {
@@ -7,5 +10,14 @@ namespace SchedulerBot.Database.Interfaces.Repositories
 	/// </summary>
 	public interface IScheduledMessageRepository : IRepository<ScheduledMessage>
 	{
+		/// <summary>
+		/// Gets the list of ScheduledMessages by <paramref name="conversationId"/> and <paramref name="state"/>.
+		/// </summary>
+		/// <param name="conversationId">The conversation identifier.</param>
+		/// <param name="state">The state.</param>
+		/// <returns></returns>
+		Task<IList<ScheduledMessage>> GetByConversationIdAndStateAsync(
+			string conversationId,
+			ScheduledMessageState state);
 	}
 }
--- SchedulerBot/SchedulerBot.Database.Repositories/Repositories/ScheduledMessageRepository.cs
+++ SchedulerBot/SchedulerBot.Database.Repositories/Repositories/ScheduledMessageRepository.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using SchedulerBot.Database.Entities;
+using SchedulerBot.Database.Entities.Enums;
 using SchedulerBot.Database.Interfaces.Repositories;
 
 namespace SchedulerBot.Database.Repositories
@@ -10,6 +15,20 @@ namespace SchedulerBot.Database.Repositories
 		public ScheduledMessageRepository(DbContext dbContext)
 			: base(dbContext)
 		{
+		}
+
+		/// <inheritdoc/>
+		public async Task<IList<ScheduledMessage>> GetByConversationIdAndStateAsync(
+			string conversationId,
+			ScheduledMessageState state)
+		{
+			List<ScheduledMessage> result = await DbSet
+				.Where(scheduledMessage =>
+					scheduledMessage.State == state
+					&& scheduledMessage.Details.ConversationId.Equals(conversationId, StringComparison.Ordinal))
+				.ToListAsync();
+
+			return result;
 		}
 	}
 }
