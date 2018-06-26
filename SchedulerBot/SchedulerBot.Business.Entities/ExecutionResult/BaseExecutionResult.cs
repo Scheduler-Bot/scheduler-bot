ed2e754852baed502d981b4b3a3a2dd01a1e7341
Alexander Kravchuk <akravch92@gmail.com>
2018-06-26 15:18:48 +0300

    Create BaseExecutionResult and make ExecutionResult inherit it.

---
+++ SchedulerBot/SchedulerBot.Business.Entities/ExecutionResult/BaseExecutionResult.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SchedulerBot.Business.Entities
+{
+	/// <summary>
+	/// Describes the base class for the result of the operation execution.
+	/// </summary>
+	public abstract class BaseExecutionResult
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BaseExecutionResult"/> class.
+		/// </summary>
+		/// <param name="errorCode">The error code.</param>
+		/// <param name="errorMessage">The error message.</param>
+		protected BaseExecutionResult(ExecutionErrorCode errorCode, string errorMessage)
+		{
+			ErrorCode = errorCode;
+			ErrorMessage = string.IsNullOrEmpty(errorMessage)
+				? errorMessage
+				: GetDescription(errorCode);
+		}
+
+		/// <summary>
+		/// Gets the ErrorCode value which is representing execution result.
+		/// </summary>
+		public ExecutionErrorCode ErrorCode { get; }
+
+		/// <summary>
+		/// Gets the error message related with the execution result.
+		/// </summary>
+		public string ErrorMessage { get; }
+
+		/// <summary>
+		/// Gets a value indicating whether the execution is successful.
+		/// </summary>
+		public bool IsSuccess => ErrorCode == ExecutionErrorCode.None;
+
+		#region Private Methods
+
+		/// <summary>
+		/// Gets the description for <param name="executionErrorCode"/>.
+		/// </summary>
+		/// <param name="executionErrorCode">The execution error code.</param>
+		/// <returns>
+		/// The result will be a string split by Upper case letters.
+		/// E.g. for InputCommandInvalidArguments result will be 'Input Command Invalid Arguments'
+		/// </returns>
+		private static string GetDescription(ExecutionErrorCode executionErrorCode)
+		{
+			string executionErrorCodeName = executionErrorCode.ToString("G");
+
+			// Splits executionErrorCodeName the by upper case letters.
+			// Solution was found by link https://stackoverflow.com/a/37532157/710014.
+			string[] words = Regex.Matches(executionErrorCodeName, "(^[a-z]+|[A-Z]+(?![a-z])|[A-Z][a-z]+)")
+				.Where(match => match != null)
+				.Select(match => match.Value)
+				.ToArray();
+			string description = string.Join(" ", words);
+
+			return description;
+		}
+
+		#endregion Private Methods
+	}
+}
--- SchedulerBot/SchedulerBot.Business.Entities/ExecutionResult/ExecutionResult.cs
+++ SchedulerBot/SchedulerBot.Business.Entities/ExecutionResult/ExecutionResult.cs
@@ -1,49 +1,17 @@
-﻿using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
-using System.Text.RegularExpressions;
-
-namespace SchedulerBot.Business.Entities
+﻿namespace SchedulerBot.Business.Entities
 {
 	/// <summary>
 	/// Describes the result of the operation execution.
 	/// </summary>
-	public class ExecutionResult
+	public sealed class ExecutionResult : BaseExecutionResult
 	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="ExecutionResult{T}"/> class.
-		/// </summary>
-		/// <param name="errorCode">The error code.</param>
-		/// <param name="errorErrorMessage">The error message.</param>
-		protected ExecutionResult(ExecutionErrorCode errorCode, string errorErrorMessage)
-		{
-			ErrorCode = errorCode;
-			ErrorMessage = string.IsNullOrEmpty(errorErrorMessage)
-				? errorErrorMessage
-				: GetDescription(errorCode);
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="ExecutionResult"/> class.
-		/// </summary>
-		protected ExecutionResult(): this(ExecutionErrorCode.None, null)
+		private ExecutionResult(ExecutionErrorCode errorCode, string errorMessage) : base(errorCode, errorMessage)
 		{
 		}
 
-		/// <summary>
-		/// Gets the ErrorCode value which is representing execution result.
-		/// </summary>
-		public ExecutionErrorCode ErrorCode { get; }
-
-		/// <summary>
-		/// Gets the error message related with the execution result.
-		/// </summary>
-		public string ErrorMessage { get; }
-
-		/// <summary>
-		/// Gets a value indicating whether the execution is successful.
-		/// </summary>
-		public bool IsSuccess => ErrorCode == ExecutionErrorCode.None;
+		private ExecutionResult(): this(ExecutionErrorCode.None, null)
+		{
+		}
 
 		/// <summary>
 		/// Performs an implicit conversion from <see cref="ExecutionErrorCode"/> to <see cref="ExecutionResult"/>.
@@ -80,59 +48,22 @@ namespace SchedulerBot.Business.Entities
 		{
 			return new ExecutionResult();
 		}
-
-		#region Private Methods
-
-		/// <summary>
-		/// Gets the description for <param name="executionErrorCode"/>.
-		/// </summary>
-		/// <param name="executionErrorCode">The execution error code.</param>
-		/// <returns>
-		/// The result will be a string split by Upper case letters.
-		/// E.g. for InputCommandInvalidArguments result will be 'Input Command Invalid Arguments'
-		/// </returns>
-		private string GetDescription(ExecutionErrorCode executionErrorCode)
-		{
-			string executionErrorCodeName = executionErrorCode.ToString("G");
-
-			// Splits executionErrorCodeName the by upper case letters.
-			// Solution was found by link https://stackoverflow.com/a/37532157/710014.
-			string[] words = Regex.Matches(executionErrorCodeName, "(^[a-z]+|[A-Z]+(?![a-z])|[A-Z][a-z]+)")
-				.Where(match => match != null)
-				.Select(match => match.Value)
-				.ToArray();
-			string description = string.Join(" ", words);
-
-			return description;
-		}
-
-		#endregion Private Methods
 	}
 
 	/// <summary>
 	/// Describes the result of the operation execution.
 	/// </summary>
-	public class ExecutionResult<T>: ExecutionResult
+	public sealed class ExecutionResult<T>: BaseExecutionResult
 		where T: class
 	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="ExecutionResult{T}"/> class.
-		/// </summary>
-		/// <param name="entity">The entity.</param>
-		private ExecutionResult(T entity)
+		private ExecutionResult(T entity) : this(ExecutionErrorCode.None, null)
 		{
 			Entity = entity;
 		}
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="ExecutionResult{T}"/> class.
-		/// </summary>
-		/// <param name="errorCode">The error code.</param>
-		/// <param name="errorErrorMessage">The error message.</param>
-		private ExecutionResult(ExecutionErrorCode errorCode, string errorErrorMessage)
-			: base(errorCode, errorErrorMessage)
+		private ExecutionResult(ExecutionErrorCode errorCode, string errorMessage)
+			: base(errorCode, errorMessage)
 		{
-			Entity = default(T);
 		}
 
 		/// <summary>
@@ -172,7 +103,7 @@ namespace SchedulerBot.Business.Entities
 		/// <returns>
 		/// A <see cref="ExecutionResult{T}" /> instance with <see cref="ExecutionResult.IsSuccess" /> set to <c>false</c>.
 		/// </returns>
-		public new static ExecutionResult<T> Error(ExecutionErrorCode errorCode, string errorMessage = null)
+		public static ExecutionResult<T> Error(ExecutionErrorCode errorCode, string errorMessage = null)
 		{
 			return new ExecutionResult<T>(errorCode, errorMessage);
 		}
